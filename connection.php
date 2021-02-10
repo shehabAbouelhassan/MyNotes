412af2f246a656645ea918d4f997011be32e2366
shehabAbouelhassan <55254824+shehabAbouelhassan@users.noreply.github.com>
2021-02-10 11:43:11 +0100

    enhancements

---
+++ connection.php
@@ -0,0 +1,10 @@
+
+
+<?php
+$link =mysqli_connect("localhost", "mynotest_notes", "root","mynotest_onlinenotes");
+if(mysqli_connect_error()){
+    die("ERROR: Unable to connect:". mysqli_connect_error());
+    echo "<script>window.alert('Hi!')</script>";
+}
+    ?>
+
